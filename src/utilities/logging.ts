eadf26195ff91251d3a692b48d375c5fafb187c3
MeridianPY <lukas.romacker@t-online.de>
2024-01-10 12:33:48 +0100

    Update logging functions

--- src/utilities/logging.ts
+++ src/utilities/logging.ts
@@ -15,42 +15,42 @@ export class logging {
   static level = levels[process.env.LOGGING_LEVEL] ?? levels.warn
   /**
    * Logs a message as an error log.
-   * @param message The message to log.
+   * @param args The message to log.
    */
-  static error(message: string) {
-    if (this.level >= levels.error) { console.error('%s \x1b[31m%s\x1b[0m', this.time(), message) }
+  static error(...args: unknown[]) {
+    if (this.level >= levels.error) { console.error('%s \x1b[31m%s\x1b[0m', this.time(), ...args) }
   }
 
   /**
    * Logs a message as a warning log.
-   * @param message The message to log.
+   * @param args The message to log.
    */
-  static warn(message: string) {
-    if (this.level >= levels.warn) { console.warn('%s \x1b[33m%s\x1b[0m', this.time(), message) }
+  static warn(...args: unknown[]) {
+    if (this.level >= levels.warn) { console.warn('%s \x1b[33m%s\x1b[0m', this.time(), ...args) }
   }
 
   /**
    * Logs a message as an info log.
-   * @param message The message to log.
+   * @param args The message to log.
    */
-  static info(message: string) {
-    if (this.level >= levels.info) { console.info('%s \x1b[34m%s\x1b[0m', this.time(), message) }
+  static info(...args: unknown[]) {
+    if (this.level >= levels.info) { console.info('%s \x1b[34m%s\x1b[0m', this.time(), ...args) }
   }
 
   /**
    * Logs a message as a success log.
-   * @param message The message to log.
+   * @param args The message to log.
    */
-  static success(message: string) {
-    console.log('%s \x1b[32m%s\x1b[0m', this.time(), message)
+  static success(...args: unknown[]) {
+    console.log('%s \x1b[32m%s\x1b[0m', this.time(), ...args)
   }
 
   /**
    * Logs a debug message (or object).
-   * @param message The message to log.
+   * @param args The message to log.
    */
-  static debug(...message: unknown[]) {
-    if (this.level >= levels.debug) { console.log('%s \x1b[36m%s\x1b[0m', this.time(), ...message) }
+  static debug(...args: unknown[]) {
+    if (this.level >= levels.debug) { console.log('%s \x1b[36m%s\x1b[0m', this.time(), ...args) }
   }
 
   /**
