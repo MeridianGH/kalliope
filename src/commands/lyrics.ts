bff93c8bdaa5d45062eb9797448c74e286ea54c2
Meridian <lukas.romacker@t-online.de>
2024-10-11 16:22:17 +0200

    revert: accidental commit from new lyrics

--- src/commands/lyrics.ts
+++ src/commands/lyrics.ts
@@ -7,11 +7,14 @@ import {
   EmbedBuilder,
   SlashCommandBuilder
 } from 'discord.js'
+import { Client as GeniusClient } from 'genius-lyrics'
+import ytdl from 'ytdl-core'
 import { genericChecks } from '../utilities/checks.js'
 import { logging } from '../utilities/logging.js'
 import { CommandStructure } from '../types/types'
 import { formatMusicFooter } from '../utilities/utilities.js'
-import { findLyrics } from '../utilities/findLyrics.js'
+
+const Genius = new GeniusClient(process.env.GENIUS_CLIENT_TOKEN)
 
 const command: CommandStructure = {
   data: new SlashCommandBuilder()
@@ -26,40 +29,30 @@ const command: CommandStructure = {
     await interaction.deferReply()
 
     const track = player.queue.current
+    const trackInfo = track.info
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    const info = await ytdl.getInfo(trackInfo.uri)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const title = info.videoDetails.media.category === 'Music' ? info.videoDetails.media.artist + ' ' + info.videoDetails.media.song : trackInfo.title
 
-    const result = await findLyrics(track)
-    const link = new URL(result.link)
+    try {
+      const search = await Genius.songs.search(title)
+      const song = search[0]
+      const lyrics = await song.lyrics()
 
-    const lines = result.lyrics.split('\n\n')
-    const pages = [`Found lyrics on [${link.host}](${link.href}):`]
-    let index = 0
-    for (const item of lines) {
-      if (pages[index].length + item.length > 4096) {
-        index++
-        pages[index] = ''
+      const lines = lyrics.split('\n')
+      const pages = ['']
+      let index = 0
+      for (const item of lines) {
+        if (pages[index].length + item.length > 4096) {
+          index++
+          pages[index] = ''
+        }
+        pages[index] += '\n' + item
       }
-      pages[index] += '\n\n' + item
-    }
 
-    const embed = new EmbedBuilder()
-      .setAuthor({
-        name: 'Lyrics.',
-        iconURL: interaction.member.displayAvatarURL()
-      })
-      .setTitle(track.info.title)
-      .setURL(track.info.uri)
-      .setThumbnail(track.info.artworkUrl)
-      .setDescription(pages[0])
-      .setFooter({
-        text: `Kalliope | ${formatMusicFooter(player)} | Found on ${link.host}.`,
-        iconURL: interaction.client.user.displayAvatarURL()
-      })
+      const isOnePage = pages.length === 1
 
-    const isOnePage = pages.length === 1
-
-    if (isOnePage) {
-      await interaction.editReply({ embeds: [embed] })
-    } else {
       const previous = new ButtonBuilder()
         .setCustomId('previous')
         .setLabel('Previous')
@@ -69,44 +62,75 @@ const command: CommandStructure = {
         .setLabel('Next')
         .setStyle(ButtonStyle.Primary)
 
-      const actionRow = isOnePage ? [] : [new ActionRowBuilder<ButtonBuilder>().setComponents([previous.setDisabled(true), next.setDisabled(false)])]
-      const message = await interaction.editReply({
-        embeds: [embed],
-        components: actionRow
-      })
-
-      // Collect button interactions (when a user clicks a button)
-      const collector = message.createMessageComponentCollector({ idle: 300000 })
-      let currentIndex = 0
-
-      const onCollect = async (buttonInteraction: ButtonInteraction<'cached'>) => {
-        if (buttonInteraction.customId === 'previous') {
-          currentIndex -= 1
-        } else {
-          currentIndex += 1
-        }
-        await buttonInteraction.update({
-          embeds: [
-            new EmbedBuilder()
-              .setAuthor({ name: 'Lyrics.', iconURL: interaction.member.displayAvatarURL() })
-              .setTitle(track.info.title)
-              .setURL(track.info.uri)
-              .setThumbnail(track.info.artworkUrl)
-              .setDescription(pages[currentIndex])
-              .setFooter({ text: `Kalliope | ${formatMusicFooter(player)} | Found on ${link.host}.`, iconURL: interaction.client.user.displayAvatarURL() })
-          ],
-          components: [new ActionRowBuilder<ButtonBuilder>().setComponents([previous.setDisabled(currentIndex === 0), next.setDisabled(currentIndex === pages.length - 1)])]
+      const embed = new EmbedBuilder()
+        .setAuthor({ name: 'Lyrics.', iconURL: interaction.member.displayAvatarURL() })
+        .setTitle(trackInfo.title)
+        .setURL(song.url)
+        .setThumbnail(trackInfo.artworkUrl)
+        .setDescription(pages[0])
+        .setFooter({
+          text: `Kalliope | ${formatMusicFooter(player)} | Provided by genius.com`,
+          iconURL: interaction.client.user.displayAvatarURL()
         })
-      }
-      collector.on('collect', (buttonInteraction: ButtonInteraction<'cached'>) => void onCollect(buttonInteraction))
 
-      const onEnd = async () => {
-        const fetchedMessage = await message.fetch(true).catch((e) => { logging.warn(`[Discord]   Failed to edit message components: ${e}`) })
-        if (!fetchedMessage) { return }
-        const disabledComponents = fetchedMessage.components[0].components.map((component: ButtonComponent) => ButtonBuilder.from(component.toJSON()).setDisabled(true))
-        await fetchedMessage.edit({ components: [new ActionRowBuilder<ButtonBuilder>().setComponents(disabledComponents)] })
+      const actionRow = isOnePage ? [] : [new ActionRowBuilder<ButtonBuilder>().setComponents([previous.setDisabled(true), next.setDisabled(false)])]
+      const message = await interaction.editReply({ embeds: [embed], components: actionRow })
+
+      if (!isOnePage) {
+        // Collect button interactions (when a user clicks a button)
+        const collector = message.createMessageComponentCollector({ idle: 300000 })
+        let currentIndex = 0
+
+        const onCollect = async (buttonInteraction: ButtonInteraction<'cached'>) => {
+          if (buttonInteraction.customId === 'previous') {
+            currentIndex -= 1
+          } else {
+            currentIndex += 1
+          }
+          await buttonInteraction.update({
+            embeds: [
+              new EmbedBuilder()
+                .setAuthor({ name: 'Lyrics.', iconURL: interaction.member.displayAvatarURL() })
+                .setTitle(trackInfo.title)
+                .setURL(song.url)
+                .setThumbnail(trackInfo.artworkUrl)
+                .setDescription(pages[currentIndex])
+                .setFooter({
+                  text: `Kalliope | ${formatMusicFooter(player)} | Provided by genius.com`,
+                  iconURL: interaction.client.user.displayAvatarURL()
+                })
+            ],
+            components: [new ActionRowBuilder<ButtonBuilder>().setComponents([previous.setDisabled(currentIndex === 0), next.setDisabled(currentIndex === pages.length - 1)])]
+          })
+        }
+        collector.on('collect', (buttonInteraction: ButtonInteraction<'cached'>) => void onCollect(buttonInteraction))
+
+        const onEnd = async () => {
+          const fetchedMessage = await message.fetch(true).catch((e) => { logging.warn(`[Discord]   Failed to edit message components: ${e}`) })
+          if (!fetchedMessage) { return }
+          const disabledComponents = fetchedMessage.components[0].components.map((component: ButtonComponent) => ButtonBuilder.from(component.toJSON()).setDisabled(true))
+          await fetchedMessage.edit({ components: [new ActionRowBuilder<ButtonBuilder>().setComponents(disabledComponents)] })
+        }
+        collector.on('end', () => void onEnd())
       }
-      collector.on('end', () => void onEnd())
+    } catch {
+      await interaction.editReply({
+        embeds: [
+          new EmbedBuilder()
+            .setAuthor({
+              name: 'Lyrics.',
+              iconURL: interaction.member.displayAvatarURL()
+            })
+            .setTitle(trackInfo.title)
+            .setURL(trackInfo.uri)
+            .setThumbnail(trackInfo.artworkUrl)
+            .setDescription('No results found!')
+            .setFooter({
+              text: `Kalliope | ${formatMusicFooter(player)} | Provided by genius.com`,
+              iconURL: interaction.client.user.displayAvatarURL()
+            })
+        ]
+      })
     }
   }
 }
